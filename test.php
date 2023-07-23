00c76f851c8f36ac7e6969e6caa59b12c519fc2f
Kelechi Nwachukwu <120751990+Gigx267@users.noreply.github.com>
2023-07-23 09:02:04 +0100

    Add files via upload

---
+++ test.php
@@ -0,0 +1,119 @@
+<!-- wp:masteriyo/certificate {"backgroundImageURL":"https://ppahs.org/wp-content/uploads/2023/04/background-image-a4-landscape.png","backgroundImageID":41774,"containerWidth":{"value":80,"unit":"%"},"paddingTop":{"value":107,"unit":"px"},"pageSize":"A4","pageOrientation":"L"} -->
+    <div id="certificate" class="certificate">
+    <div class="certificate-content-wrapper"><!-- wp:columns -->
+    <div class="wp-block-columns"><!-- wp:column {"width":"25%"} -->
+    <div class="wp-block-column" style="flex-basis: 25%;"> </div>
+    <!-- /wp:column -->
+    
+    <!-- wp:column {"width":"50%"} -->
+    <div class="wp-block-column" style="flex-basis: 50%;"> </div>
+    <!-- /wp:column -->
+    
+    <!-- wp:column {"width":"25%"} --></div>
+    <!-- /wp:spacer -->
+    
+    <!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"60px","letterSpacing":"-0.4%","textTransform":"uppercase"},"color":{"text":"#272727"}}} -->
+    <p>&nbsp;</p>
+    <p class="has-text-align-center has-text-color" style="color:blue; font-size: 60px; text-transform: uppercase; letter-spacing: -0.4%;"><span style="text-decoration: underline;"><span style="font-size: 18pt;"><strong>PHYSICIAN - PATIENT ALLIANCE FOR HEALTH &amp; sAFETY </strong></span></span></p>
+    <table style="border-collapse: collapse; width: 100%; height: 10px;">
+    <tbody>
+    <tr style="height: 10px;">
+    <td style="width: 100%; height: 10px;"> </td>
+    </tr>
+    </tbody>
+    </table>
+    <!-- /wp:paragraph --> <br /><!-- wp:paragraph {"align":"center","style":{"typography":{"fontSize":"60px","letterSpacing":"-0.4%","textTransform":"uppercase"},"color":{"text":"#272727"}}} -->
+    <p>&nbsp;</p>
+    <p class="has-text-align-center has-text-color" style="color: blue; font-size: 60px; text-transform: uppercase; letter-spacing: -0.4%;"><strong><span style="font-size: 14pt;"><em>AN ACCREDITED PROVIDER OF CONTINUING MEDICAL EDUCATION BY THE CPD   </em> <em>STANDARDS OFFICE  </em></span></strong></p>
+    <table style="border-collapse: collapse; width: 100%; height: 10px;">
+    <tbody>
+    <tr style="height: 10px;">
+    <td style="width: 100%; height: 10px;"> </td>
+    </tr>
+    </tbody>
+    </table>
+    <!-- /wp:paragraph -->
+    
+    <!-- wp:spacer {"height":"14px"} -->
+    <div class="wp-block-spacer" style="height: 14px;" aria-hidden="true"> </div>
+    <!-- /wp:spacer -->
+    
+    <!-- wp:spacer {"height":"4px"} -->
+    <div class="wp-block-spacer" style="height: 4px;" aria-hidden="true"> </div>
+    <!-- /wp:spacer -->
+    
+    <!-- wp:columns {} -->
+    <div class="wp-block-columns"><!-- wp:column {"width":"25%"} -->
+    <div class="wp-block-column" style="flex-basis: 25%;"> </div>
+    <!-- /wp:column -->
+    
+    <!-- wp:column {"width":"50%"} -->
+    <div class="wp-block-column" style="flex-basis: 50%;"><!-- wp:paragraph {"align":"center","style":{"typography":{"textTransform":"uppercase","letterSpacing":"5px"},"color":{"text":"#272727"}},"fontSize":"medium"} -->
+    <p class="has-text-align-center has-text-color has-medium-font-size" style="color: #272727; text-transform: uppercase; letter-spacing: 5px;"> </p>
+    <!-- /wp:paragraph --></div>
+    <!-- /wp:column -->
+    
+    <!-- wp:column {"width":"25%"} -->
+    <div class="wp-block-column" style="flex-basis: 25%;"> </div>
+    <!-- /wp:column --></div>
+    <!-- /wp:columns -->
+    
+    <!-- wp:spacer {"height":"32px"} -->
+    <div class="wp-block-spacer" style="height: 32px;" aria-hidden="true"> </div>
+    <!-- /wp:spacer -->
+    
+    <!-- wp:paragraph {"align":"center","style":{"color":{"text":"#606060"}}} -->
+    <p class="has-text-align-center has-text-color" style="color: #606060;">This Certifies That</p>
+    <!-- /wp:paragraph -->
+    
+    <!-- wp:spacer {"height":"32px"} -->
+    <div class="wp-block-spacer" style="height: 32px;" aria-hidden="true"> </div>
+    <!-- /wp:spacer -->
+    
+    <!-- wp:masteriyo/student-name {"clientId":"48fd1b40","blockCSS":".masteriyo-student-name-blocku002du002d48fd1b40 { text-align: center; }n.masteriyo-student-name-blocku002du002d48fd1b40 { font-size: 42px; }n.masteriyo-student-name-blocku002du002d48fd1b40 { color: #272727; }","alignment":"center","fontSize":{"value":42,"unit":"px"},"textColor":"#272727"} -->
+    <div class="wp-block-masteriyo-student-name masteriyo-student-name-block--48fd1b40">{{masteriyo_student_name}}</div>
+    <!-- /wp:masteriyo/student-name -->
+    
+    <!-- wp:spacer {"height":"40px"} -->
+    <div class="wp-block-spacer" style="height: 40px;" aria-hidden="true"> </div>
+    <!-- /wp:spacer -->
+    
+    <!-- wp:paragraph {"align":"center","style":{"color":{"text":"#606060"}}} -->
+    <p class="has-text-align-center has-text-color" style="color: #606060;">Has Passed The Educational Course</p>
+    <!-- /wp:paragraph -->
+    
+    <!-- wp:spacer {"height":"26px"} -->
+    <div class="wp-block-spacer" style="height: 26px;" aria-hidden="true"> </div>
+    <!-- /wp:spacer -->
+    
+    <!-- wp:masteriyo/course-title {"clientId":"b6454285","blockCSS":".masteriyo-course-title-blocku002du002db6454285 { text-align: center; }n.masteriyo-course-title-blocku002du002db6454285 { font-size: 24px; }n.masteriyo-course-title-blocku002du002db6454285 { color: #272727; }","alignment":"center","fontSize":{"value":24,"unit":"px"},"textColor":"#272727"} -->
+    <div class="wp-block-masteriyo-course-title masteriyo-course-title-block--b6454285">{{masteriyo_course_title}}</div>
+    <!-- /wp:masteriyo/course-title -->
+    
+    <!-- wp:spacer {"height":"75px"} -->
+    <div class="wp-block-spacer" style="height: 75px;" aria-hidden="true"> </div>
+    <!-- /wp:spacer -->
+    
+    <!-- wp:columns -->
+    <div class="wp-block-columns">
+    <div class="wp-block-column" style="flex-basis: 35%;">
+    <div class="wp-block-spacer" style="height: 38px;" aria-hidden="true"> </div>
+    <div class="wp-block-masteriyo-course-completion-date masteriyo-course-completion-date-block--c4ab5a05">{{masteriyo_course_completion_date}}</div>
+    <div class="wp-block-spacer" style="height: 10px;" aria-hidden="true"> </div>
+    <hr class="wp-block-separator has-text-color has-alpha-channel-opacity has-background is-style-wide" style="background-color: #434343; color: #434343;" />
+    <div class="wp-block-spacer" style="height: 8px;" aria-hidden="true"> </div>
+    <p class="has-text-align-center has-text-color" style="color: #272727;">Date</p>
+    </div>
+    <div class="wp-block-column" style="flex-basis: 30%;"> </div>
+    <div class="wp-block-column" style="flex-basis: 35%;">
+    <figure class="wp-block-image aligncenter size-full is-resized"><img class="wp-image-41779" src="https://ppahs.org/wp-content/uploads/2023/04/signature.png" alt="" width="153" height="53" /></figure>
+    <div class="wp-block-spacer" style="height: 10px;" aria-hidden="true"> </div>
+    <hr class="wp-block-separator has-text-color has-alpha-channel-opacity has-background is-style-wide" style="background-color: #434343; color: #434343;" />
+    <div class="wp-block-spacer" style="height: 8px;" aria-hidden="true"> </div>
+    <p class="has-text-align-center has-text-color" style="color: #272727;">Signature</p>
+    </div>
+    </div>
+    <p>&nbsp;</p>
+    <!-- /wp:columns --></div>
+    </div>
+    <!-- /wp:masteriyo/certificate -->
